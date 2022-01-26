19ab4d4bb13ba11c2c8bfbb37c4be1efe65c11c2
Bishal Khanal <bishal.khanal87@gmail.com>
2022-01-26 15:10:34 -0800

    added js

---
+++ index.js
@@ -0,0 +1,15 @@
+let person = prompt("What is your Name?");
+alert("Hello" + person + "Welcome to Bishal's Page");
+let hob = prompt("What is Bishal's Hobby");
+if(hob == 'Travel')
+{
+    prompt("You Are Correct!")
+}
+     else if(hob =='Music')
+    {
+        prompt("You are correct!")
+    }
+    else {
+        prompt("sorry try again")
+    }
+
